64d76ab088d568f2196bbf53fbf24406aa6b4116
barroco <barroco@ebu.ch>
2014-01-27 12:55:26 +0100

    Remove useless tests and models

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,4 @@ module.exports = function(app) {
   app.get('/', function(req, res) {
     res.render('index.ejs');
   });
-
-  app.put('/user', function(req, res) {
-    db.User
-      .create({
-        username: req.body.name,
-        password: 'hello'
-      })
-      .complete(function(err, user) {
-        if (err) {
-          res.send(500);
-        }
-        else {
-          res.send(200, user);
-        }
-      });
-  });
 };
