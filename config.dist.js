e757df7e8edc1f3a777253eba981236fd55e8770
barroco <barroco@ebu.ch>
2014-01-28 14:15:37 +0100

    Update environment variables

--- config.dist.js
+++ config.dist.js
@@ -19,5 +19,5 @@ exports.uris = {
   authorization_uri: "http://example.com/authorized"
 };
 
-exports.service_provider_id = process.env.SP_ID || '1';
-exports.service_provider_name = process.env.SP_NAME || 'STATION_NAME';
+exports.service_provider_id = process.env.SERVICE_PROVIDER_ID || '1';
+exports.service_provider_name = process.env.SERVICE_PROVIDER_NAME || 'STATION_NAME';
