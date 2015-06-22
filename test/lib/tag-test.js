ee603c8c783a5a0934470f9aa2d2965a4223538a
Chris Needham <chris.needham@bbc.co.uk>
2015-06-22 11:23:40 +0100

    Added XPath object to group related functions

--- test/lib/tag-test.js
+++ test/lib/tag-test.js
@@ -56,27 +56,28 @@ var resetDatabase = function(done) {
   return dbHelper.resetDatabase(initDatabase, done);
 };
 
-var selectXPathNode = function(doc, path) {
-  var nodes = xpath.select(path, doc);
-  expect(nodes.length).to.equal(1, "xpath " + path + " returned no elements");
+var XPath = {
+  selectNode: function(doc, path) {
+    var nodes = xpath.select(path, doc);
+    expect(nodes.length).to.equal(1, "xpath " + path + " returned no elements");
+    return nodes[0];
+  },
 
-  return nodes[0];
-};
+  verifyNodeValue: function(doc, path, value) {
+    var node = this.selectNode(doc, path);
+    expect(node.firstChild.data).to.equal(value);
+  },
 
-var verifyXPathNodeValue = function(doc, path, value) {
-  var node = selectXPathNode(doc, path);
-  expect(node.firstChild.data).to.equal(value);
-};
+  verifyAttribute: function(doc, path, value) {
+    var node = this.selectNode(doc, path);
+    expect(node.nodeValue).to.equal(value);
+  },
 
-var verifyXPathAttribute = function(doc, path, value) {
-  var node = selectXPathNode(doc, path);
-  expect(node.nodeValue).to.equal(value);
-};
-
-var verifyXPathNodeHasNamespace = function(doc, path, uri, prefix) {
-  var node = selectXPathNode(doc, path);
-  var actualPrefix = node.lookupPrefix(uri);
-  expect(actualPrefix).to.equal(prefix);
+  verifyNodeHasNamespace: function(doc, path, uri, prefix) {
+    var node = this.selectNode(doc, path);
+    var actualPrefix = node.lookupPrefix(uri);
+    expect(actualPrefix).to.equal(prefix);
+  }
 };
 
 describe("GET /radiodns/tag/1/tags", function() {
@@ -111,33 +112,33 @@ describe("GET /radiodns/tag/1/tags", function() {
       });
 
       it("should reference the atom namespace", function() {
-        verifyXPathNodeHasNamespace(this.doc, '/feed', 'http://www.w3.org/2005/Atom', '');
+        XPath.verifyNodeHasNamespace(this.doc, '/feed', 'http://www.w3.org/2005/Atom', '');
       });
 
       it("should reference the radiotag namespace", function() {
-        verifyXPathNodeHasNamespace(this.doc, '/feed', 'http://radiodns.org/2011/radiotag', 'radiotag');
+        XPath.verifyNodeHasNamespace(this.doc, '/feed', 'http://radiodns.org/2011/radiotag', 'radiotag');
       });
 
       it("should contain a title", function() {
-        verifyXPathNodeValue(this.doc, '/feed/title', 'Tag List');
+        XPath.verifyNodeValue(this.doc, '/feed/title', 'Tag List');
       });
 
       it("should have a self link url", function() {
-        verifyXPathAttribute(this.doc, "/feed/link[@rel='self']/@href", 'http://example.com/clients/11/tags');
+        XPath.verifyAttribute(this.doc, "/feed/link[@rel='self']/@href", 'http://example.com/clients/11/tags');
       });
 
       describe("the updated date", function() {
         it("should be the time of the most recent tag", function() {
-          verifyXPathNodeValue(this.doc, '/feed/updated', '2014-01-29T17:50:12Z');
+          XPath.verifyNodeValue(this.doc, '/feed/updated', '2014-01-29T17:50:12Z');
         });
       });
 
       it("should have an author", function() {
-        verifyXPathNodeValue(this.doc, '/feed/author/name', 'BBC1');
+        XPath.verifyNodeValue(this.doc, '/feed/author/name', 'BBC1');
       });
 
       it("should have a unique identifier", function() {
-        verifyXPathNodeValue(this.doc, '/feed/id', 'urn:radiotag:client:11');
+        XPath.verifyNodeValue(this.doc, '/feed/id', 'urn:radiotag:client:11');
       });
 
       it("should contain an entry for each of the user's tags", function() {
@@ -148,74 +149,74 @@ describe("GET /radiodns/tag/1/tags", function() {
       describe("the entries", function() {
         describe("the title field", function() {
           it("should include the tag id", function() {
-            verifyXPathNodeValue(this.doc, '/feed/entry[1]/title', 'Tag: radio2 at 2014-01-29T17:50:12Z');
-            verifyXPathNodeValue(this.doc, '/feed/entry[2]/title', 'Tag: radio1 at 2014-01-29T17:50:11Z');
-            verifyXPathNodeValue(this.doc, '/feed/entry[3]/title', 'Tag: radio3 at 2014-01-29T17:50:10Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry[1]/title', 'Tag: radio2 at 2014-01-29T17:50:12Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry[2]/title', 'Tag: radio1 at 2014-01-29T17:50:11Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry[3]/title', 'Tag: radio3 at 2014-01-29T17:50:10Z');
           });
         });
 
         describe("the radiotag:service field uri attribute", function() {
           it("should equal the tag's radio service bearer URI", function() {
-            verifyXPathAttribute(this.doc, "/feed/entry[1]/*[local-name(.)='service']/@uri", 'radio2');
-            verifyXPathAttribute(this.doc, "/feed/entry[2]/*[local-name(.)='service']/@uri", 'radio1');
-            verifyXPathAttribute(this.doc, "/feed/entry[3]/*[local-name(.)='service']/@uri", 'radio3');
+            XPath.verifyAttribute(this.doc, "/feed/entry[1]/*[local-name(.)='service']/@uri", 'radio2');
+            XPath.verifyAttribute(this.doc, "/feed/entry[2]/*[local-name(.)='service']/@uri", 'radio1');
+            XPath.verifyAttribute(this.doc, "/feed/entry[3]/*[local-name(.)='service']/@uri", 'radio3');
           });
         });
 
         describe("the radiotag:service field", function() {
           it("should equal the tag's radio service name", function() {
             // TODO: should be station display local-name
-            verifyXPathNodeValue(this.doc, "/feed/entry[1]/*[local-name(.)='service']", 'radio2');
-            verifyXPathNodeValue(this.doc, "/feed/entry[2]/*[local-name(.)='service']", 'radio1');
-            verifyXPathNodeValue(this.doc, "/feed/entry[3]/*[local-name(.)='service']", 'radio3');
+            XPath.verifyNodeValue(this.doc, "/feed/entry[1]/*[local-name(.)='service']", 'radio2');
+            XPath.verifyNodeValue(this.doc, "/feed/entry[2]/*[local-name(.)='service']", 'radio1');
+            XPath.verifyNodeValue(this.doc, "/feed/entry[3]/*[local-name(.)='service']", 'radio3');
           });
         });
 
         describe("the id field", function() {
           it("should contain the tag identifier", function() {
-            verifyXPathNodeValue(this.doc, '/feed/entry[1]/id', 'urn:uuid:4575bf06-9919-4237-9eb7-8c4f42885774');
-            verifyXPathNodeValue(this.doc, '/feed/entry[2]/id', 'urn:uuid:cc5355c3-93f1-4616-9a54-9093a0c656fc');
-            verifyXPathNodeValue(this.doc, '/feed/entry[3]/id', 'urn:uuid:e39ec3f6-09c9-4b58-82c1-70782aa8ad7c');
+            XPath.verifyNodeValue(this.doc, '/feed/entry[1]/id', 'urn:uuid:4575bf06-9919-4237-9eb7-8c4f42885774');
+            XPath.verifyNodeValue(this.doc, '/feed/entry[2]/id', 'urn:uuid:cc5355c3-93f1-4616-9a54-9093a0c656fc');
+            XPath.verifyNodeValue(this.doc, '/feed/entry[3]/id', 'urn:uuid:e39ec3f6-09c9-4b58-82c1-70782aa8ad7c');
           });
         });
 
         describe("the updated field", function() {
           it("should equal the tag time", function() {
-            verifyXPathNodeValue(this.doc, '/feed/entry[1]/updated', '2014-01-29T17:50:12Z');
-            verifyXPathNodeValue(this.doc, '/feed/entry[2]/updated', '2014-01-29T17:50:11Z');
-            verifyXPathNodeValue(this.doc, '/feed/entry[3]/updated', '2014-01-29T17:50:10Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry[1]/updated', '2014-01-29T17:50:12Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry[2]/updated', '2014-01-29T17:50:11Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry[3]/updated', '2014-01-29T17:50:10Z');
           });
         });
 
         describe("the published field", function() {
           it("should equal the tag time", function() {
-            verifyXPathNodeValue(this.doc, '/feed/entry[1]/published', '2014-01-29T17:50:12Z');
-            verifyXPathNodeValue(this.doc, '/feed/entry[2]/published', '2014-01-29T17:50:11Z');
-            verifyXPathNodeValue(this.doc, '/feed/entry[3]/published', '2014-01-29T17:50:10Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry[1]/published', '2014-01-29T17:50:12Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry[2]/published', '2014-01-29T17:50:11Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry[3]/published', '2014-01-29T17:50:10Z');
           });
         });
 
         describe("the summary field", function() {
           it("should include the tag description", function() {
-            verifyXPathNodeValue(this.doc, '/feed/entry[1]/summary', 'Description of tag: radio2 at 2014-01-29T17:50:12Z');
-            verifyXPathNodeValue(this.doc, '/feed/entry[2]/summary', 'Description of tag: radio1 at 2014-01-29T17:50:11Z');
-            verifyXPathNodeValue(this.doc, '/feed/entry[3]/summary', 'Description of tag: radio3 at 2014-01-29T17:50:10Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry[1]/summary', 'Description of tag: radio2 at 2014-01-29T17:50:12Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry[2]/summary', 'Description of tag: radio1 at 2014-01-29T17:50:11Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry[3]/summary', 'Description of tag: radio3 at 2014-01-29T17:50:10Z');
           });
         });
 
         describe("the image link field", function() {
           it("should include an image url", function() {
-            verifyXPathAttribute(this.doc, "/feed/entry[1]/link[@rel='image']/@href", 'http://example.com/image.png');
-            verifyXPathAttribute(this.doc, "/feed/entry[2]/link[@rel='image']/@href", 'http://example.com/image.png');
-            verifyXPathAttribute(this.doc, "/feed/entry[3]/link[@rel='image']/@href", 'http://example.com/image.png');
+            XPath.verifyAttribute(this.doc, "/feed/entry[1]/link[@rel='image']/@href", 'http://example.com/image.png');
+            XPath.verifyAttribute(this.doc, "/feed/entry[2]/link[@rel='image']/@href", 'http://example.com/image.png');
+            XPath.verifyAttribute(this.doc, "/feed/entry[3]/link[@rel='image']/@href", 'http://example.com/image.png');
           });
         });
 
         describe("the canonical link field", function() {
           it("should include a url", function() {
-            verifyXPathAttribute(this.doc, "/feed/entry[1]/link[@rel='canonical']/@href", 'http://example.com/radio2/1391017812');
-            verifyXPathAttribute(this.doc, "/feed/entry[2]/link[@rel='canonical']/@href", 'http://example.com/radio1/1391017811');
-            verifyXPathAttribute(this.doc, "/feed/entry[3]/link[@rel='canonical']/@href", 'http://example.com/radio3/1391017810');
+            XPath.verifyAttribute(this.doc, "/feed/entry[1]/link[@rel='canonical']/@href", 'http://example.com/radio2/1391017812');
+            XPath.verifyAttribute(this.doc, "/feed/entry[2]/link[@rel='canonical']/@href", 'http://example.com/radio1/1391017811');
+            XPath.verifyAttribute(this.doc, "/feed/entry[3]/link[@rel='canonical']/@href", 'http://example.com/radio3/1391017810');
           });
         });
       });
@@ -288,32 +289,32 @@ describe("POST /radiodns/tag/1/tag", function() {
       });
 
       it("should reference the atom namespace", function() {
-        verifyXPathNodeHasNamespace(this.doc, '/feed', 'http://www.w3.org/2005/Atom', '');
+        XPath.verifyNodeHasNamespace(this.doc, '/feed', 'http://www.w3.org/2005/Atom', '');
       });
 
       it("should reference the radiotag namespace", function() {
-        verifyXPathNodeHasNamespace(this.doc, '/feed', 'http://radiodns.org/2011/radiotag', 'radiotag');
+        XPath.verifyNodeHasNamespace(this.doc, '/feed', 'http://radiodns.org/2011/radiotag', 'radiotag');
       });
 
       it("should contain a title", function() {
-        verifyXPathNodeValue(this.doc, '/feed/title', 'Tag List');
+        XPath.verifyNodeValue(this.doc, '/feed/title', 'Tag List');
       });
 
       it("should have an author", function() {
-        verifyXPathNodeValue(this.doc, '/feed/author/name', 'BBC1');
+        XPath.verifyNodeValue(this.doc, '/feed/author/name', 'BBC1');
       });
 
       it("should contain a unique identifier", function() {
-        verifyXPathNodeValue(this.doc, '/feed/id', 'urn:radiotag:client:12');
+        XPath.verifyNodeValue(this.doc, '/feed/id', 'urn:radiotag:client:12');
       });
 
       it("should have a self link url", function() {
-        verifyXPathAttribute(this.doc, "/feed/link[@rel='self']/@href", 'http://example.com/clients/12/tags');
+        XPath.verifyAttribute(this.doc, "/feed/link[@rel='self']/@href", 'http://example.com/clients/12/tags');
       });
 
       describe("the updated date", function() {
         it("should be the time of the tag", function() {
-          verifyXPathNodeValue(this.doc, '/feed/updated', '2014-01-29T07:48:13Z');
+          XPath.verifyNodeValue(this.doc, '/feed/updated', '2014-01-29T07:48:13Z');
         });
       });
 
@@ -325,20 +326,20 @@ describe("POST /radiodns/tag/1/tag", function() {
       describe("the entry", function() {
         describe("the title field", function() {
           it("should include the tag id", function() {
-            verifyXPathNodeValue(this.doc, '/feed/entry/title', 'Tag: radio1 at 2014-01-29T07:48:13Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry/title', 'Tag: radio1 at 2014-01-29T07:48:13Z');
           });
         });
 
         describe("the radiotag:service uri attribute", function() {
           it("should equal the tag's service bearer URI", function() {
-            verifyXPathAttribute(this.doc, "/feed/entry/*[local-name(.)='service']/@uri", 'radio1');
+            XPath.verifyAttribute(this.doc, "/feed/entry/*[local-name(.)='service']/@uri", 'radio1');
           });
         });
 
         describe("the radiotag:service field", function() {
           it("should equal the tag's station name", function() {
             // TODO: should be station display local-name
-            verifyXPathNodeValue(this.doc, "/feed/entry/*[local-name(.)='service']", 'radio1');
+            XPath.verifyNodeValue(this.doc, "/feed/entry/*[local-name(.)='service']", 'radio1');
           });
         });
 
@@ -352,31 +353,31 @@ describe("POST /radiodns/tag/1/tag", function() {
 
         describe("the updated field", function() {
           it("should equal the tag time", function() {
-            verifyXPathNodeValue(this.doc, '/feed/entry/updated', '2014-01-29T07:48:13Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry/updated', '2014-01-29T07:48:13Z');
           });
         });
 
         describe("the published field", function() {
           it("should equal the tag time", function() {
-            verifyXPathNodeValue(this.doc, '/feed/entry/published', '2014-01-29T07:48:13Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry/published', '2014-01-29T07:48:13Z');
           });
         });
 
         describe("the summary field", function() {
           it("should include the tag description", function() {
-            verifyXPathNodeValue(this.doc, '/feed/entry/summary', 'Description of tag: radio1 at 2014-01-29T07:48:13Z');
+            XPath.verifyNodeValue(this.doc, '/feed/entry/summary', 'Description of tag: radio1 at 2014-01-29T07:48:13Z');
           });
         });
 
         describe("the image link field", function() {
           it("should include an image url", function() {
-            verifyXPathAttribute(this.doc, "/feed/entry/link[@rel='image']/@href", 'http://example.com/image.png');
+            XPath.verifyAttribute(this.doc, "/feed/entry/link[@rel='image']/@href", 'http://example.com/image.png');
           });
         });
 
         describe("the canonical link field", function() {
           it("should include a url", function() {
-            verifyXPathAttribute(this.doc, "/feed/entry/link[@rel='canonical']/@href", 'http://example.com/radio1/1390981693');
+            XPath.verifyAttribute(this.doc, "/feed/entry/link[@rel='canonical']/@href", 'http://example.com/radio1/1390981693');
           });
         });
       });
